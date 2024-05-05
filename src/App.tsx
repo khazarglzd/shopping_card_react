1dab399a87c2ca6c15ac87f42e865fab28876045
khazarglzd <khazarguluzade@gmail.com>
2024-05-05 15:36:17 +0400

    adding routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
-function App() {
-  return <h1>Hi</h1>;
-}
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
+
+const App = () => {
+  return (
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
+};
 
 export default App;
