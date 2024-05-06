bcfc01452d070b9d71d6edec8d32ff6d441c04d4
khazarglzd <khazarguluzade@gmail.com>
2024-05-06 15:20:14 +0400

    creating Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
 
 const App = () => {
   return (
-    <Container className="mb-4">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
+    </>
   );
 };
 
